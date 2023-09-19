978e7810f624d2b3d00bb5c9271b61a41f460bc6
jangjh0201 <jangjh0201@gmail.com>
2023-09-19 12:56:11 +0900

    ✅ add : add test feature

--- app/src/test/java/navigation/Dijkstra.java
+++ app/src/test/java/navigation/Dijkstra.java
@@ -2,6 +2,7 @@ package navigation;
 
 import java.util.List;
 
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -27,5 +28,16 @@ public class Dijkstra {
 
         assertThat(path).containsExactly("v1", "v2", "v3");
         assertThat(distance).isEqualTo(2);
+    }
+
+    @Test
+    public void noneConnectedPath() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        DijkstraShortestPath<String, DefaultWeightedEdge> shortestPath = new DijkstraShortestPath<>(graph);
+        GraphPath<String, DefaultWeightedEdge> path = shortestPath.getPath("v3", "v1");
+        assertThat(path).isNull();
     }
 }
